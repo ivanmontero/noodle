f3425a6fbf2c09a18fee2303181f33a6a0a100ea
Demo User <ivanspmontero@gmail.com>
2017-07-28 11:17:01 -0700

    Began making the basic structure of the website

--- main.py
+++ main.py
@@ -1,24 +1,49 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+# TODO:
+# - Split each website into it's own .py file
+
+# Features:
+# - About us
+# - Calendar
+# - Search (General, Photos, etc)
+# - Games
+# - Photos
+# - Checkers
+# - Social Media
+# - Blog ("The Noodle")
+# - Noodle News
+
+# Suggestions:
+# - Use Bootstrap for CSS
+
+# ========================= LIBRARIES =========================
+# Core libraries
 import webapp2
+import os
+import sys
+import jinja2
+import logging
+# Parsing libraries
+import json
+import urllib2
+# Authentification and databasse libraries
+from google.appengine.api import users
+from google.appengine.ext import ndb
+# To allow Python to import from handlers/ folder
+# sys.path.append('/handlers')
+# Other pages
+# from handlers.index import *
+# TODO
+
+# jinja initialization
+jinja_environment = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        # self.response.write('Hello world!')
+        # template = jinja_environment.get_template('templates/index.html')
+        # self.response.out.write(template.render())
+        # test_function()
 
 
 
