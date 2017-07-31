cf81a6ab6af6c21a33751b7db4aca170fc2cb491
Demo User <ivanspmontero@gmail.com>
2017-07-30 19:31:09 -0700

    Added authentification

--- index.py
+++ index.py
@@ -12,10 +12,30 @@ from google.appengine.ext import ndb
 # Get jinja environment from main
 from main import jinja_environment
 
+# Google+ API Key (for getting user image): AIzaSyBTXhazY7-0epr_F1ardo8CHOA9afRXfuc
+
 class IndexHandler(webapp2.RequestHandler):
     def get(self):
+        user = users.get_current_user()
+        data = { 
+            "logged_in" : True if user else False,
+            # Tests whether server is on Google's cloud or localhost
+            "cloud_hosted" : os.getenv('SERVER_SOFTWARE', '').startswith('Google App Engine/')
+        }
+        if user:
+            data["logout_url"] = users.create_logout_url('/')
+            data["user_nickname"] = user.nickname()
+            data["user_id"] = user.user_id()
+            # Testing purposes, My ID: 114783711473680853748
+            if(data["cloud_hosted"]):
+                data["user_image"] = json.loads(urllib2.urlopen("https://www.googleapis.com/plus/v1/people/" 
+                    + str(user.user_id()) + "?fields=image&key=AIzaSyBTXhazY7-0epr_F1ardo8CHOA9afRXfuc").read())["image"]["url"]
+            else:
+                data["user_image"] = "/res/img/anon-pic.png"
+        else:
+            data["login_url"] = users.create_login_url('/')
         template = jinja_environment.get_template('templates/index.html')
-        self.response.out.write(template.render())
+        self.response.out.write(template.render(data))
     
     def post(self):
         pass
