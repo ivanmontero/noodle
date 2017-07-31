e7f34371b54960cdbf5f0ae417d6aaf2a0b45ddc
Demo User <ivanspmontero@gmail.com>
2017-07-31 09:24:39 -0700

    Created webpage for question forum

--- questions.py
+++ questions.py
@@ -14,7 +14,7 @@ from main import jinja_environment
 
 class QuestionHandler(webapp2.RequestHandler):
     def get(self):
-        template = jinja_environment.get_template('templates/your_template_name_here.html')
+        template = jinja_environment.get_template('templates/questions.html')
         self.response.out.write(template.render())
     
     def post(self):
